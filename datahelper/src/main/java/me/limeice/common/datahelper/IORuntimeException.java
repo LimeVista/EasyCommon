53e139986ef8caf885dd31896f721b90114b8bdc
LimeVista <LimeVista@outlook.com>
2018-04-06 03:48:40 +0800

    移除Data Helper

--- datahelper/src/main/java/me/limeice/common/datahelper/IORuntimeException.java
+++
@@ -1,9 +0,0 @@
-package me.limeice.common.datahelper;
-
-public class IORuntimeException extends RuntimeException {
-
-    public IORuntimeException(Throwable t) {
-        super("IOException->", t);
-    }
-
-}
