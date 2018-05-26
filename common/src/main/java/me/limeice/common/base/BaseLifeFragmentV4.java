43ec370ef38931714dfc6552fd77c60a8d4fb457
Lime <LimeVista@outlook.com>
2018-05-26 13:31:00 +0800

    添加LifeFragment

---
+++ common/src/main/java/me/limeice/common/base/BaseLifeFragmentV4.java
@@ -0,0 +1,30 @@
+package me.limeice.common.base;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.CallSuper;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BaseLifeFragmentV4 extends Fragment {
+
+    @Nullable
+    @Override
+    @CallSuper
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        LifeFragmentV4.getLifeFragment(inflater.getContext()).addFragment(this);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Context context = getContext();
+        if (context != null)
+            LifeFragmentV4.getLifeFragment(context).removeFragment(this);
+    }
+}
